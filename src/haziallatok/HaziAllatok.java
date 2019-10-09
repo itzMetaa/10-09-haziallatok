9a2009073f6fb9792f8e3eefeedc94534d261a73
Diak <Diak@10.4.117.8>
2019-10-09 14:31:52 +0200

    Publish

--- src/haziallatok/HaziAllatok.java
+++ src/haziallatok/HaziAllatok.java
@@ -1,12 +1,24 @@
 
 package haziallatok;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HaziAllatok {
 
 
     public static void main(String[] args) {
         Dog gyilkos = new Dog("gyilkos","Blöki");
         Fish arany = new Fish("xd");
+        
+        List<Pet> haziAllatLista = new ArrayList<>();
+        haziAllatLista.add(arany);
+        haziAllatLista.add(gyilkos);
+        
+        for (Pet pet : haziAllatLista) {
+            pet.Description();
+            pet.Voice();
+        }
     }
     
 }
