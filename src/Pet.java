6b3b81336119620d4b9cf1d78e1fabee1dce40d1
Diak <Diak@10.4.117.8>
2019-10-09 14:02:12 +0200

    init()

---
+++ src/Pet.java
@@ -0,0 +1,11 @@
+public class Pet {
+    
+    private String name;
+    private int numberOfLegs;
+    
+    public Pet(String name, int numberOfLegs){
+        this.name = name;
+        this.numberOfLegs = numberOfLegs;
+    }
+    
+}
---
+++ src/haziallatok/HaziAllatok.java
@@ -0,0 +1,11 @@
+
+package haziallatok;
+
+public class HaziAllatok {
+
+
+    public static void main(String[] args) {
+ 
+    }
+    
+}
